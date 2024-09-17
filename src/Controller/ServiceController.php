e8945f0ceb8ef9d63ddb5a814ff04fdbb5c8495d
span16 <Khiari.chaima@esprit.tn>
2024-09-17 11:39:01 +0100

    First project

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ServiceController extends AbstractController
+{
+    #[Route('/service/{name}', name: 'app_service', defaults:['name' => null])]
+    public function index($name): Response
+    {
+        return $this->render('service/showservice.html.twig', [
+            'name' => $name,
+        ]);
+    }
+}
+
